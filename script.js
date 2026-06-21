7e92c120c07f640d72c18b494fa2042ead3e8dcf
SEO JISU <seogs1004@naver.com>
2026-06-22 00:00:24 +0900

    시작과 설명 추가

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-
 const canvas = document.getElementById("gameCanvas");
-
 const ctx = canvas.getContext("2d");
 
-const scoreEl = document.getElementById("score");
-
-const bestEl = document.getElementById("best");
-
-const livesEl = document.getElementById("lives");
+const startScreen = document.getElementById("startScreen");
+const startBtn = document.getElementById("startBtn");
 
 const overlay = document.getElementById("overlay");
 
-const finalScore = document.getElementById("finalScore");
+const restartBtn =
+document.getElementById("restartBtn");
 
-const finalBest = document.getElementById("finalBest");
+const scoreEl =
+document.getElementById("score");
 
-const restartBtn = document.getElementById("restartBtn");
+const bestEl =
+document.getElementById("best");
+
+const livesEl =
+document.getElementById("lives");
+
+const finalScore =
+document.getElementById("finalScore");
+
+const finalBest =
+document.getElementById("finalBest");
 
 
 
@@ -25,15 +32,24 @@ let meteors=[];
 
 let stars=[];
 
-let lives;
 
-let score;
+let score=0;
 
-let best;
+let best=0;
+
+let lives=3;
+
+let gameStarted=false;
+
+let gameOver=false;
 
 let invincible=0;
 
-let gameOver=false;
+
+
+let leftPressed=false;
+
+let rightPressed=false;
 
 
 
@@ -50,7 +66,7 @@ width:50,
 
 height:50,
 
-speed:8
+speed:10
 
 };
 
@@ -61,7 +77,7 @@ meteors=[];
 stars=[];
 
 
-for(let i=0;i<50;i++){
+for(let i=0;i<70;i++){
 
 stars.push({
 
@@ -76,14 +92,10 @@ size:Math.random()*2+1
 }
 
 
-
-lives=3;
-
-
 score=0;
 
 
-best=localStorage.getItem("meteorBest") || 0;
+lives=3;
 
 
 invincible=0;
@@ -92,23 +104,31 @@ invincible=0;
 gameOver=false;
 
 
+best=
+
+localStorage.getItem("meteorBest") || 0;
+
+
 overlay.classList.add("hidden");
 
 
-updateHud();
+updateUI();
 
 }
 
 
 
-function updateHud(){
+function updateUI(){
 
 
 livesEl.innerHTML=
 
-"❤️".repeat(lives)+
+"❤️".repeat(lives)
+
++
 
 "🤍".repeat(3-lives);
+
 
 
 scoreEl.innerText=score;
@@ -116,53 +136,140 @@ scoreEl.innerText=score;
 
 bestEl.innerText=best;
 
+
 }
 
 
 
-document.addEventListener("keydown",(e)=>{
+startBtn.addEventListener(
+
+"click",
+
+()=>{
+
+
+gameStarted=true;
+
+
+startScreen.style.display="none";
+
+
+init();
+
+
+}
+
+);
+
+
+
+restartBtn.addEventListener(
+
+"click",
+
+()=>{
+
+
+init();
+
+
+}
+
+);
+
+
+
+
+document.addEventListener(
+
+"keydown",
+
+(e)=>{
 
 
 if(e.key==="ArrowLeft"){
 
-player.x-=player.speed;
+leftPressed=true;
 
 }
 
 
 if(e.key==="ArrowRight"){
 
-player.x+=player.speed;
+rightPressed=true;
 
 }
 
 
-});
+}
+
+);
+
+
+
+
+document.addEventListener(
+
+"keyup",
+
+(e)=>{
+
+
+if(e.key==="ArrowLeft"){
+
+leftPressed=false;
+
+}
+
+
+if(e.key==="ArrowRight"){
+
+rightPressed=false;
+
+}
+
+
+}
+
+);
+
 
 
 
 setInterval(()=>{
 
 
+if(!gameStarted) return;
+
+
 if(gameOver) return;
+
 
 
 meteors.push({
 
+
 x:Math.random()*450,
+
 
 y:-50,
 
+
 radius:20+Math.random()*20,
+
 
 speed:3+Math.random()*3,
 
+
 passed:false
+
 
 });
 
 
-},550);
+},500);
+
+
 
 
 
@@ -172,17 +279,27 @@ function collision(a,b){
 
 return (
 
+
 a.x < b.x+b.radius*2 &&
+
 
 a.x+a.width > b.x &&
 
+
 a.y < b.y+b.radius*2 &&
+
 
 a.y+a.height > b.y
 
+
 );
 
+
 }
+
+
+
+
 
 
 
@@ -193,6 +310,7 @@ ctx.fillStyle="white";
 
 
 stars.forEach(star=>{
+
 
 star.y+=1;
 
@@ -216,13 +334,21 @@ star.size
 
 );
 
+
 });
+
 
 }
 
 
 
+
+
+
+
+
 function drawPlayer(){
+
 
 
 if(
@@ -250,33 +376,52 @@ ctx.beginPath();
 
 ctx.moveTo(player.x+25,player.y);
 
+
 ctx.lineTo(player.x,player.y+50);
+
 
 ctx.lineTo(player.x+50,player.y+50);
 
+
 ctx.closePath();
+
 
 ctx.fill();
 
+
 }
+
+
+
+
 
 
 
 function drawMeteors(){
 
 
+
 for(let i=meteors.length-1;i>=0;i--){
+
 
 let m=meteors[i];
 
 
-m.y+=m.speed+(score*0.01);
+
+m.y +=
+
+m.speed +
+
+score*0.02;
+
+
 
 
 ctx.fillStyle="#f97316";
 
 
 ctx.beginPath();
+
 
 ctx.arc(
 
@@ -292,20 +437,35 @@ Math.PI*2
 
 );
 
+
 ctx.fill();
 
 
 
-if(m.y>700 && !m.passed){
+
+if(
+
+m.y>700
+
+&&
+
+!m.passed
+
+){
+
 
 m.passed=true;
+
 
 score++;
 
 
+
 if(score>best){
 
+
 best=score;
+
 
 localStorage.setItem(
 
@@ -315,40 +475,65 @@ best
 
 );
 
-}
-
-
-updateHud();
 
 }
 
 
 
-if(collision(player,m) && invincible<=0){
+updateUI();
+
+
+}
+
+
+
+
+if(
+
+collision(player,m)
+
+&&
+
+invincible<=0
+
+){
+
 
 
 lives--;
 
 
+
 invincible=60;
 
 
-updateHud();
+
+updateUI();
+
 
 
 
 if(lives<=0){
 
+
 gameOver=true;
 
 
-overlay.classList.remove("hidden");
+
+overlay.classList.remove(
+
+"hidden"
+
+);
+
 
 
 finalScore.innerText=score;
 
 
 finalBest.innerText=best;
+
+
 
 }
 
@@ -358,18 +543,51 @@ finalBest.innerText=best;
 
 if(m.y>760){
 
+
 meteors.splice(i,1);
 
-}
-
-
-}
 
 }
 
 
 
-function update(){
+}
+
+
+}
+
+
+
+
+
+
+
+function gameLoop(){
+
+
+
+requestAnimationFrame(
+
+gameLoop
+
+);
+
+
+
+if(
+
+!gameStarted
+
+||
+
+gameOver
+
+){
+
+return;
+
+}
+
 
 
 ctx.clearRect(
@@ -378,15 +596,32 @@ ctx.clearRect(
 
 0,
 
-canvas.width,
+500,
 
-canvas.height
+700
 
 );
 
 
 
 drawStars();
+
+
+
+
+if(leftPressed){
+
+player.x-=player.speed;
+
+}
+
+
+
+if(rightPressed){
+
+player.x+=player.speed;
+
+}
 
 
 
@@ -406,11 +641,13 @@ player.x
 
 
 
+
 drawPlayer();
 
 
 
 drawMeteors();
+
 
 
 
@@ -422,26 +659,8 @@ invincible--;
 
 
 
-requestAnimationFrame(update);
-
 }
 
 
 
-restartBtn.addEventListener(
-
-"click",
-
-()=>{
-
-init();
-
-}
-
-);
-
-
-
-init();
-
-update();
+gameLoop();
